be6564e99c5356b3c914c664371ac8661d3080e0
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-09-24 19:34:48 -0500

    finished wizard mockup

--- static/character_creator.jsx
+++ static/character_creator.jsx
@@ -27,21 +27,21 @@ class ChararacterCreatorApp extends React.Component {
                                 </span>
                             </div>
                             <div id="dwarfCollapse" className="collapse">
-                                <div className="d-flex flex-sm-row flex-column">
+                                <div className="d-flex flex-sm-row flex-column" style={{justifyContent: "space-between"}}>
                                     <div>
                                         <p>Bold and hardy, dwarves are known as skilled warriors, miners, and workers of stone and metal. Though they stand well under 5 feet tall, dwarves are so broad and compact that they can weigh as much as a human standing nearly two feet taller. Their courage and endurance are also easily a match for any of the larger folk.</p>
                                         <div><b>Key traits:</b>
                                             <ul>
-                                                <li>Usual alignment:<span className="badge bg-success"><i className="fas fa-balance-scale"></i> Lawful good</span></li>
-                                                <li>Ability score perks:<span className="badge bg-success"><i className="fas fa-star"></i> +2 Constitution</span></li>
-                                                <li>Proficiencies:
-                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Battleaxe</span>
-                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Handaxe</span>
-                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Light hammer</span>
-                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Warhammer</span>
+                                                <li><b>Usual alignment:</b><span className="badge bg-warning text-dark"><i className="fas fa-balance-scale"></i> Lawful good</span></li>
+                                                <li><b>Ability score perks:</b><span className="badge bg-light text-success"><i className="fas fa-star"></i> +2 Constitution</span></li>
+                                                <li><b>Proficiencies:</b>
+                                                    <span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Battleaxe</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Handaxe</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Light hammer</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Warhammer</span>
                                                     <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Artisan's Tools (1)</span>
                                                 </li>
-                                                <li>Other traits:
+                                                <li><b>Other traits:</b>
                                                     <span className="badge bg-primary"><i className="fas fa-eye"></i> Darkvision</span>
                                                     <span className="badge bg-primary"><i className="fas fa-eye"></i> Dwarven Resilience</span>
                                                     <span className="badge bg-primary"><i className="fas fa-eye"></i> Stonecunning</span>
@@ -54,7 +54,7 @@ class ChararacterCreatorApp extends React.Component {
                                                 <div className="card-body selectable-card">
                                                     <h6 className="card-title">Hill Dwarf</h6>
                                                     <div className="mb-3">
-                                                        <span className="badge bg-success"><i className="fas fa-star"></i> +1 Wisdom</span>
+                                                        <span className="badge bg-light text-success"><i className="fas fa-star"></i> +1 Wisdom</span>
                                                         <span className="badge bg-primary"><i className="fas fa-eye"></i> Dwarven Toughness</span>
                                                     </div>
                                                     <p>As a hill dwarf, you have keen senses, deep intuition, and remarkable resilience.</p>
@@ -64,9 +64,9 @@ class ChararacterCreatorApp extends React.Component {
                                                 <div className="card-body selectable-card">
                                                     <h6 className="card-title">Mountain Dwarf</h6>
                                                     <div className="mb-3">
-                                                        <span className="badge bg-success"><i className="fas fa-star"></i> +2 Strength</span>
-                                                        <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Light Armor</span>
-                                                        <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Medium Armor</span>
+                                                        <span className="badge bg-light text-success"><i className="fas fa-star"></i> +2 Strength</span>
+                                                        <span className="badge bg-secondary"><i className="fas fa-shield-alt"></i> Light Armor</span>
+                                                        <span className="badge bg-secondary"><i className="fas fa-shield-alt"></i> Medium Armor</span>
                                                     </div>
                                                     <p>As a mountain dwarf, you’re strong and hardy, accustomed to a difficult life in rugged terrain.</p>
                                                 </div>
@@ -79,7 +79,67 @@ class ChararacterCreatorApp extends React.Component {
                         </div>
                     </div>
                     <h2 className="mt-3">❷ Choose a class</h2>
-
+                    <div className="card selectable-card">
+                        <div className="card-body d-flex flex-column">
+                            <div className="d-flex flex-row">
+                                <h5 className="card-title">
+                                    <span className="me-3">Wizard</span>
+                                </h5>
+                                <span className="link-primary" data-bs-toggle="collapse" href="#wizardCollapse" role="button" aria-expanded="false" aria-controls="wizardCollapse">
+                                    [Toggle info]
+                                </span>
+                            </div>
+                            <div id="wizardCollapse" className="collapse">
+                                <div className="d-flex flex-sm-row flex-column" style={{justifyContent: "space-between"}}>
+                                    <div>
+                                        <p>A scholarly magic-user capable of manipulating the structures of reality.</p>
+                                        <div><b>Key traits:</b>
+                                            <ul>
+                                                <li><b>Hit dice:</b><span className="badge bg-danger"><i className="fas fa-bolt"></i> 1d6</span></li>
+                                                <li>
+                                                    <b>Hit points:</b><span className="badge bg-success"><i className="fas fa-heart"></i> 6 + Constitution Modifier</span>
+                                                </li>
+                                                <li><b>Proficiencies:</b>
+                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Dagger</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Darts</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Sling</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Quarterstaff</span>
+                                                    <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Light Crossbow</span>
+                                                </li>
+                                                <li><b>Saving throws:</b>
+                                                    <span className="badge bg-light text-dark"><i className="fas fa-star"></i> Intelligence</span>
+                                                    <span className="badge bg-light text-dark"><i className="fas fa-star"></i> Wisdom</span>
+                                                </li>
+                                                <li><b>Skills:</b> Choose two from 
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> Arcana</span>
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> History</span>
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> Insight</span>
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> Investigation</span>
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> Medicine</span>
+                                                    <span className="badge bg-dark"><i className="fas fa-book"></i> Religion</span>
+                                                </li>
+                                                <li><b>Magic:</b>
+                                                    <span className="badge bg-primary"><i className="fas fa-fire"></i> Cantrip (3)</span>
+                                                    <span className="badge bg-primary"><i className="fas fa-fire"></i> 1st lv Wizard Spell (6)</span>
+                                                </li>
+                                                <li><b>Starting equipment:</b>
+                                                    <ul>
+                                                        <li><span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Quarterstaff</span> or <span className="badge bg-secondary"><i className="fas fa-fist-raised"></i> Dagger</span></li>
+                                                        <li><span className="badge bg-secondary"><i className="fas fa-hammer"></i> Component Pouch</span> or <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Arcane Focus</span></li>
+                                                        <li><span className="badge bg-secondary"><i className="fas fa-hammer"></i> Scholar's Pack</span> or <span className="badge bg-secondary"><i className="fas fa-hammer"></i> Explorer's Pack</span></li>
+                                                        <li><span className="badge bg-secondary"><i className="fas fa-hammer"></i> Spellbook</span></li>
+                                                    </ul>
+                                                    <div>Alternatively, start with <b class="text-warning">4d4 x 10gp</b> and buy equipment of your choice.</div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <img className="race-image" src="https://www.dndbeyond.com/avatars/thumbnails/6/357/420/618/636272696881281556.png"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className="mt-3">❸ Roll for ability scores</h2>
                 </form>
             </div>
         );
